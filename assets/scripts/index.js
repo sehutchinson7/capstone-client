14c37809764096b292a2966250a2fd1830c0b93f
sehutchinson7 <sehutchinson7@gmail.com>
2018-05-26 16:21:36 -0400

    Add javascript function `addCurrentLocationToMap` in index.js to geoolocate  a user's location using latitude and longitude. Add button to html  "click here to get location". Add jquery to index.js so when a user  clicks the button they are asked whether they would like to enable  location in their browser. If a user consents, they are then shown a pop  up on the map, which shows their approximate location. Currently, the map  is confined find to the geographic boundary of Massachusetts.
     Next steps:
        1. Still need to remove stylesheet link tag from html and follow the
        steps outlined in the previous commit to support vector tile rendering.
        2. Add a marker to the map with the user's location.

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -11,15 +11,41 @@ const L = require('leaflet')
 
 $(() => {
   $(document).ready(function () {
+    const addCurrentLocationToMap = function () {
+      // if browser supports geolocation, get the position
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+          const lat = position.coords.latitude
+          const long = position.coords.longitude
+          console.log('--- Your Position: ---')
+          console.log('Lat: ' + position.coords.latitude)
+          console.log('Long: ' + position.coords.longitude)
+          console.log('-----------------------')
+
+          // let marker = L.marker([position.coords.latitude, position.coords.longitude].addTo(map)
+          // add marker to the map
+          const marker = L.marker([lat, long]).addTo(map);
+          // add a pop up to tell the user their approximate location
+          marker.bindPopup('Hi Sara, you are somewhere close to this location').openPopup()
+        })
+      // if geolocation is not supported
+      } else {
+        console.log('Geolocation not enabled or supported')
+      }
+    }
     // `map` is an element, which is the same as the empty div in the html
     // `L` is the leaflet library that lets you do thing
     const map = L.map('map').setView([42.35, -71.08], 13) // lat, lon, zoom
 
-    L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
-      {
-        attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
-        maxZoom: 17,
-        minZoom: 9
-      }).addTo(map)
+    L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png', {
+      attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+      maxZoom: 17,
+      minZoom: 9
+    }).addTo(map)
+
+    // Adds current location using html5
+    $('#get-location').click(function (event) {
+      addCurrentLocationToMap()
+    })
   })
 })
