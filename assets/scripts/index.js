e7da521dbe1e2de0ba9278a71fff026c5596c692
sehutchinson7 <sehutchinson7@gmail.com>
2018-05-27 22:58:04 -0400

    Testing mixed content in deployment

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -44,7 +44,7 @@ $(() => {
 
     L.tileLayer('http://tiles.mapc.org/basemap/{z}/{x}/{y}.png',
       {
-        attribution: 'Tiles by <a href="http://mapc.org">MAPC</a>, Data by <a href="http://mass.gov/mgis">MassGIS</a>',
+        attribution: 'Tiles by <a href="https://mapc.org">MAPC</a>, Data by <a href="https://mass.gov/mgis">MassGIS</a>',
         maxZoom: 17,
         minZoom: 9
       }).addTo(map)
